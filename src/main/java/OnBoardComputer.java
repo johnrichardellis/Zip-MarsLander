7825ad2e2f81b826e875c8627c2d075f3fc78380
Ellis John <ellisjohn@zipcodes-MacBook-Pro-chuck.local>
2021-11-29 07:08:30 -0500

    keep trying

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -64,19 +64,37 @@ public class OnBoardComputer implements BurnStream {
 //        350		1 		7501 		1		100
 
 
-
-
-
-
-
-
-
-
-
-
         System.out.println(burn); /*hack!*/
         return burn;
 
+
+
+
+
+//        int altitude = status.getAltitude();
+//        int velocity = status.getVelocity();
+//        int burn = (velocity * velocity/ (2 * altitude)) + 100;
+//
+//        if (altitude < 10000 && altitude >= 5000) { burn = 150; }
+//
+//        if (altitude < 5000 && altitude >= 1000) {
+//            if (velocity > 200) { burn = 200; }
+//            else { burn = 100; } }
+//
+//        if (altitude < 1000 && altitude >= 200) {
+//            if (velocity > 200) { burn = 200; }
+//            else { burn = 100; } }
+//
+//        if (altitude < 200 && altitude >= 1) {
+//            if (velocity > 1000) { burn = 200; }
+//            if (velocity > 200 && velocity <= 1000) { burn = 200; }
+//            if (velocity > 50 && velocity <= 200) { burn = ((velocity - 50) + 100); }
+//            if (velocity > 10 && velocity <= 50) { burn = ((velocity - 10) + 100); }
+//            if (velocity > 0 && velocity <= 10) { burn = ((velocity - 1) + 100); } }
+//
+//
+//        System.out.println(burn); /*hack!*/
+//        return burn;
 
     }
 }
